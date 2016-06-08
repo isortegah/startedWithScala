804d58e0c9fda43056659434ef2ad33ac10b1683
isortegah <isortegah@gmail.com>
2016-06-07 23:05:11 -0500

    Solución funciona max

--- curso/Functions & Evaluation/Ejemplo Assignment/example/src/main/scala/example/Lists.scala
+++ curso/Functions & Evaluation/Ejemplo Assignment/example/src/main/scala/example/Lists.scala
@@ -41,5 +41,21 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
-  }
+    def max(xs: List[Int]): Int = {
+
+     if (xs.isEmpty) throw new NoSuchElementException
+     else {
+       def inner(max: Int, list: List[Int]): Int = {
+   
+         def compare(x: Int, y: Int): Int =
+           if (x > y) x
+           else y
+   
+         if (list.isEmpty) max
+         else inner(compare(max, list.head), list.tail)
+       }
+       inner(xs.head, xs.tail)
+     } 
+   }
+   
+}
